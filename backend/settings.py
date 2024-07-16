811b80aaa0fa2afdd576d4f303f19c04360716d7
zakhij <zakhijaouy@gmail.com>
2024-07-16 09:08:11 -0700

    debug .env config

--- backend/settings.py
+++ backend/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config, Csv
+from decouple import config, Csv
 
-config = Config()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
