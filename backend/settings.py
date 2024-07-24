1a8b239c73962d84379ad48a0fd93e8931b989c0
zakhij <zakhijaouy@gmail.com>
2024-07-24 12:43:23 -0700

    index

--- backend/settings.py
+++ backend/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR)],
+        'DIRS': [os.path.join(BASE_DIR / 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ git pull
 # Navigate to frontend directory and build the project
 echo "Building frontend..."
 cd frontend
+npm install
 npm run build
-
 echo "Copying build contents to static directory..."
 cp -r build/* ../
 
 echo "Running collectstatic..."
-cd ../backend
+cd ..
 python manage.py collectstatic --noinput
 
 echo "Deployment complete"
