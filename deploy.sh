b3e0bd1d554a9bcb1eba027b66f1a73831105c88
zakhij <zakhijaouy@gmail.com>
2024-07-24 16:32:22 -0700

    update statfiles

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ cp -r build/* ../
 
 echo "Copying static contents to static directory..."
 cd ..
-mv manifest.json /staticfiles
-mv favicon.ico /staticfiles
-mv logo* /staticfiles
-mv asset-manifest.json /staticfiles
+mv manifest.json ./staticfiles
+mv favicon.ico ./staticfiles
+mv logo* ./staticfiles
+mv asset-manifest.json ./staticfiles
 
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
