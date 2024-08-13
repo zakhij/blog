28422b75219ffa2d9b66b0ee4908a518a1a3919c
zakhij <zakhijaouy@gmail.com>
2024-08-13 13:45:06 -0700

    email html template + send to each user individually

--- app/emails.py
+++ app/emails.py
@@ -4,10 +4,13 @@ from django.utils.html import strip_tags
 
 def send_new_blog_email(blog, subscribers):
     subject = f"New Blog Post: {blog.title}"
-    #html_message = render_to_string('new_blog.html', {'blog': blog})
-    html_message = "<p>New Blog Post: <a href='https://signalinthehaystack.xyz/blogposts/{blog.slug}'>{blog.title}</a></p>"
-    #plain_message = strip_tags(html_message)
-    plain_message = f"New Blog Post: {blog.title}"
-    to_list = [subscriber.email for subscriber in subscribers]
-
-    send_mail(subject=subject, from_email=None, message=plain_message,recipient_list=to_list, html_message=html_message)
+    html_message = render_to_string('new_blog.html', {'blog': blog})
+    plain_message = strip_tags(html_message)
+    for subscriber in subscribers:
+        send_mail(
+            subject=subject,
+            message=plain_message,
+            from_email=None,
+            recipient_list=[subscriber.email],
+            html_message=html_message
+        )
