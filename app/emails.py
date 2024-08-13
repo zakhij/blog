51a4620f23842659bf6b395da7a35de1ca865d59
zakhij <zakhijaouy@gmail.com>
2024-08-13 13:19:16 -0700

    update emails.py to leverage default settings

--- app/emails.py
+++ app/emails.py
@@ -8,7 +8,6 @@ def send_new_blog_email(blog, subscribers):
     html_message = "<p>New Blog Post: <a href='https://signalinthehaystack.xyz/blogposts/{blog.slug}'>{blog.title}</a></p>"
     #plain_message = strip_tags(html_message)
     plain_message = f"New Blog Post: {blog.title}"
-    from_email = 'zakhijaouy@gmail.com'
     to_list = [subscriber.email for subscriber in subscribers]
 
-    send_mail(subject, plain_message, from_email, to_list, html_message=html_message)
+    send_mail(subject=subject, from_email=None, message=plain_message,recipient_list=to_list, html_message=html_message)
